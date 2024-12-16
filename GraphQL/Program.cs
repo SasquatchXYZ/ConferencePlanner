db277d0fb8db3ac4bc5e8f12cfba7044eccb76ea
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-15 22:51:12 -0500

    Wire up GraphQL in Program.cs

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -4,9 +4,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddDbContext<ApplicationDbContext>( // Registers the `ApplicationDbContext` service so that it can be injected into resolvers
-        options => options.UseNpgsql("Host=127.0.0.1;Username=graphql_workshop;Password=secret"));
+    // Registers the `ApplicationDbContext` service so that it can be injected into resolvers
+    .AddDbContext<ApplicationDbContext>(
+        options => options.UseNpgsql("Host=127.0.0.1;Username=graphql_workshop;Password=secret"))
+    .AddGraphQLServer();
 
 var app = builder.Build();
 
-app.Run();
+app.MapGraphQL();
+
+await app.RunWithGraphQLCommandsAsync(args);
