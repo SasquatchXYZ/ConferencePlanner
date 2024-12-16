c6784198c1c4725b32bfc7694e0df66cb12c6eda
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-15 23:00:17 -0500

    Adding a Query

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -7,7 +7,11 @@ builder.Services
     // Registers the `ApplicationDbContext` service so that it can be injected into resolvers
     .AddDbContext<ApplicationDbContext>(
         options => options.UseNpgsql("Host=127.0.0.1;Username=graphql_workshop;Password=secret"))
-    .AddGraphQLServer();
+    .AddGraphQLServer()
+    // This registers all types in the assembly using a source generator (`HotChocolate.Types.Analyzers`)
+    // The name of the `AddGraphQLTypes` method is based on the assembly name by default,
+    // but can be changed using the `[Module]` attribute on the assembly.
+    .AddGraphQLTypes();
 
 var app = builder.Build();
 
