4b09f135dea77346277e38e29fee736b0073bc8a
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-16 10:25:21 -0500

    Add a query that uses the dataloader

--- GraphQL/Queries.cs
+++ GraphQL/Queries.cs
@@ -1,4 +1,6 @@
 using ConferencePlanner.GraphQL.Data;
+using GreenDonut.Selectors;
+using HotChocolate.Execution.Processing;
 using Microsoft.EntityFrameworkCore;
 
 namespace ConferencePlanner.GraphQL;
@@ -6,9 +8,20 @@ namespace ConferencePlanner.GraphQL;
 public static class Queries
 {
     [Query]
-    public static async Task<IEnumerable<Speaker>> GetSpeakersAsync(ApplicationDbContext dbContext,
+    public static async Task<IEnumerable<Speaker>> GetSpeakersAsync(
+        ApplicationDbContext dbContext,
         CancellationToken cancellationToken)
     {
         return await dbContext.Speakers.AsNoTracking().ToListAsync(cancellationToken);
+    }
+
+    [Query]
+    public static async Task<Speaker?> GetSpeakerAsync(
+        int id,
+        ISpeakerByIdDataLoader speakerByIdDataLoader,
+        ISelection selection,
+        CancellationToken cancellationToken)
+    {
+        return await speakerByIdDataLoader.Select(selection).LoadAsync(id, cancellationToken);
     }
 }
