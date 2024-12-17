3459039a1d21fefbc9e304798e0fa4bc4e87a8be
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-17 16:20:17 -0500

    Add/register filtering and sorting

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -16,6 +16,8 @@ builder.Services
     .AddDbContextCursorPagingProvider()
     // This makes paging arguments available to resolver methods
     .AddPagingArguments()
+    .AddFiltering()
+    .AddSorting()
     // This registers all types in the assembly using a source generator (`HotChocolate.Types.Analyzers`)
     // The name of the `AddGraphQLTypes` method is based on the assembly name by default,
     // but can be changed using the `[Module]` attribute on the assembly.
