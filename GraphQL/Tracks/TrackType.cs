223fe71907938b2ab354dacf2f2bc9837fcdc1fc
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-17 11:24:37 -0500

    Add a UseUpper middleware

--- GraphQL/Tracks/TrackType.cs
+++ GraphQL/Tracks/TrackType.cs
@@ -1,4 +1,5 @@
 using ConferencePlanner.GraphQL.Data;
+using ConferencePlanner.GraphQL.Extensions;
 using GreenDonut.Selectors;
 using HotChocolate.Execution.Processing;
 
@@ -7,6 +8,14 @@ namespace ConferencePlanner.GraphQL.Tracks;
 [ObjectType<Track>]
 public static partial class TrackType
 {
+    static partial void Configure(IObjectTypeDescriptor<Track> descriptor)
+    {
+        descriptor
+            .Field(track => track.Name)
+            .ParentRequires(nameof(Track.Name))
+            .UseUpperCase();
+    }
+
     public static async Task<IEnumerable<Session>> GetSessionsAsync(
         [Parent] Track track,
         ISessionsByTrackIdDataLoader sessionsByTrackIdDataLoader,
