e3e3693637dd19f5a512dc22f6562ed1c0bdb96f
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-16 22:20:33 -0500

    Update SpeakerQueries to add plural queries

--- GraphQL/Speakers/SpeakerQueries.cs
+++ GraphQL/Speakers/SpeakerQueries.cs
@@ -16,12 +16,20 @@ public static class SpeakerQueries
     }
 
     [NodeResolver] // Marks the node resolver for a Relay node type.  It will also set the GraphQL type of the `id` parameter to `ID`
-    public static async Task<Speaker?> GetSpeakerAsync(
+    public static async Task<Speaker?> GetSpeakerByIdAsync(
         int id,
         ISpeakerByIdDataLoader speakerByIdDataLoader,
         ISelection selection,
         CancellationToken cancellationToken)
     {
         return await speakerByIdDataLoader.Select(selection).LoadAsync(id, cancellationToken);
+    }
+
+    public static async Task<IEnumerable<Speaker>> GetSpeakersByIdAsync(
+        [ID<Speaker>] int[] ids,
+        ISpeakerByIdDataLoader speakerByIdDataLoader,
+        CancellationToken cancellationToken)
+    {
+        return await speakerByIdDataLoader.LoadRequiredAsync(ids, cancellationToken);
     }
 }
