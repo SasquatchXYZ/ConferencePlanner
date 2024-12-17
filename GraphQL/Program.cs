e4f81f07d37c13e59624e1b7d8747c3532613c6b
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-17 12:19:18 -0500

    Add and register pagination

--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -12,6 +12,8 @@ builder.Services
     // This enables the mutation conventions to minimize boilerplate code.  Instead of manually creating payload
     // types, Hot Chocolate can generate these types for us automatically.
     .AddMutationConventions()
+    // Adds the cursor paging provider to the schema configuration that uses native keyset pagination
+    .AddDbContextCursorPagingProvider()
     // This registers all types in the assembly using a source generator (`HotChocolate.Types.Analyzers`)
     // The name of the `AddGraphQLTypes` method is based on the assembly name by default,
     // but can be changed using the `[Module]` attribute on the assembly.
