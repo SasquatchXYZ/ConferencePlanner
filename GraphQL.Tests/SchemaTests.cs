6650f1027353141d7b9f305b41886b5e49f74da3
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-18 11:02:34 -0500

    Add schema changed test

---
+++ GraphQL.Tests/SchemaTests.cs
@@ -0,0 +1,33 @@
+﻿using ConferencePlanner.GraphQL.Data;
+using CookieCrumble;
+using HotChocolate.Execution;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQL.Tests;
+
+public sealed class SchemaTests
+{
+    [Fact]
+    public async Task SchemaChanged()
+    {
+        // Arrange & Act
+        // Takes the service collection and builds a schema from it
+        var schema = await new ServiceCollection()
+            .AddDbContext<ApplicationDbContext>()
+            .AddGraphQLServer()
+            .AddGlobalObjectIdentification()
+            .AddMutationConventions()
+            .AddDbContextCursorPagingProvider()
+            .AddPagingArguments()
+            .AddFiltering()
+            .AddSorting()
+            .AddInMemorySubscriptions()
+            .AddGraphQLTypes()
+            .BuildSchemaAsync();
+
+        // Assert
+        // Creates a snapshot of the GraphQL SDL representation of the schema,
+        // which is compared in subsequent test runs
+        schema.MatchSnapshot();
+    }
+}
